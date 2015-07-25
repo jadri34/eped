c8d0afffbadcff68580ad91f14fd415ca1d39a33
alex <alex@pdp7.net>
2015-07-25 10:50:58 +0200

    ComparatorIF, IteratorIF y ListIF

---
+++ eped/eped/base/original/IteratorIF.java
@@ -0,0 +1,25 @@
+package eped.base.original;
+
+public interface IteratorIF<T> {
+
+	/**
+	 * Devuelve el siguiente elemento de la iteracion.
+	 * @return el siguiente elemento de la iteracion.
+	 */
+
+	public T getNext();
+
+	/**
+	 * Indica si hay más elemento en la iteración.
+	 * @return true si hay más elemento en la iteración
+	 */
+
+	public boolean hasNext();
+
+	/**
+	 * Restablece el iterador para volver al inicio.
+	 */
+
+	public void reset();
+
+}
